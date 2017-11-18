c5fe3676755d957e22e664b36be52d46ba0ce019
Lee Robinson <lee.robinson@workiva.com>
2017-11-17 21:38:18 -0600

    Run coverage with pub serve

--- tool/dev.dart
+++ tool/dev.dart
@@ -1,34 +1,20 @@
 library tool.dev;
 
-import 'package:dart_dev/dart_dev.dart' show TestRunnerConfig, config, dev;
+import 'package:dart_dev/dart_dev.dart' show config, dev;
 
 main(List<String> args) async {
   // https://github.com/Workiva/dart_dev
 
   var directories = ['lib/', 'tool/', 'web/'];
   config.analyze.entryPoints = directories;
-  config.coverage.reportOn = ['lib/'];
+
+  config.coverage
+    ..reportOn = ['lib/']
+    ..pubServe = true;
+
   config.format
     ..paths = directories
     ..lineLength = 100;
-
-  config.genTestRunner.configs = <TestRunnerConfig>[
-    new TestRunnerConfig(
-        genHtml: true,
-        directory: 'test/unit',
-        filename: 'generated_runner_test',
-        dartHeaders: const <String>[
-          'import "package:over_react/over_react.dart";',
-        ],
-        preTestCommands: const <String>[
-          'setClientConfiguration();',
-          'enableTestMode();',
-        ],
-        htmlHeaders: const <String>[
-          '<script src="packages/react/react_with_addons.js"></script>',
-          '<script src="packages/react/react_dom.js"></script>',
-        ])
-  ];
 
   config.test
     ..platforms = ['content-shell']
