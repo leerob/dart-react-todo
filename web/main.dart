8a4eebb1c8cc34cf9555e05d5783699393f4b324
Aaron Lademann <aaron.lademann@workiva.com>
2019-09-19 07:00:10 -0700

    Update to stable version of over_react 2.x
    + And upgrade all other dependencies to latest and greatest

--- web/main.dart
+++ web/main.dart
@@ -1,15 +1,15 @@
 import 'dart:html';
 
-import 'package:react/react_dom.dart' as react_dom;
-import 'package:react/react_client.dart' as react_client;
+import 'package:over_react/over_react.dart';
+import 'package:over_react/react_dom.dart' as react_dom;
 
 import 'package:todo_dart_react/todo_dart_react.dart';
 
 void main() {
-  react_client.setClientConfiguration();
+  setClientConfiguration();
 
-  TodoActions actions = new TodoActions();
-  TodoStore store = new TodoStore(actions);
+  TodoActions actions = TodoActions();
+  TodoStore store = TodoStore(actions);
 
   var todoApp = (TodoApp()
     ..actions = actions
